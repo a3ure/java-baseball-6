67f58eeb86abd4aa44ecd09cf059ed82e72c7241
a3ure <148525725+a3ure@users.noreply.github.com>
2023-12-01 22:38:43 +0900

    feat : 랜덤 숫자 생성 기능 작성

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -2,26 +2,37 @@ package baseball.controller;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import baseball.util.RandomNumberGenerator;
+import baseball.validation.InputValidation;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import java.util.List;
 
 public class BaseballController {
 
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
+    InputValidation inputValidation = new InputValidation();
+    RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
     public void Game(){
         outputView.printStartMessage();
         getUserNumber();
+        List<Integer> computerNumbers = getRandomNumbers();
     }
 
     private void getUserNumber(){
         inputView.askNumber();
         String userNumber = readLine();
+        try{
+            inputValidation.validateInputNumber(userNumber);
+        }catch (IllegalArgumentException e){
+            getUserNumber();
+        }
     }
 
-
-
-
+    private List<Integer> getRandomNumbers(){
+        return randomNumberGenerator.generateRandomNumbers();
+    }
 
 }
