b8b4c7b7565fbb1cf0aa1f4685ca47c677731f35
a3ure <148525725+a3ure@users.noreply.github.com>
2023-12-01 19:48:02 +0900

    feat : InputView, OutView 작성
    입력, 출력 시에 필요한 문자열 저장

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,6 @@
+package baseball.view;
+
+public class InputView {
+    private static final String ASK_USER_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String ASK_NEW_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class OutputView {
+    private static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String BALL ="볼";
+    private static final String STRIKE ="스트라이크";
+    private static final String NOTHING ="낫싱";
+    private static final String ENDING_MESSAGE = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료";
+}
